992bdc827ef4d383fa0165aa7119c979c48cc206
Brian Lewis <brian@lorf.org>
2013-05-05 08:13:31 +0800

    add default values in load_conf

--- lib/reaver-tools.rb
+++ lib/reaver-tools.rb
@@ -3,7 +3,14 @@ require "yaml"
 module ReaverTools
   def self.load_conf
     file_name = File.join(ENV["HOME"], ".reaver-tools.conf")
-    YAML.load(File.open(file_name))
+    YAML.load(File.open(file_name)).tap do |h|
+      h["cache_dir"] ||= File.join(ENV["HOME"], "reaver-cache")
+      h["list"] ||= {}
+      h["list"]["num_sec"]   ||= 60
+      h["list"]["temp_name"] ||= "reaver-list-temp"
+      h["run"] ||= {}
+      h["run"]["extra_args"] ||= []
+    end
   end
 
   def self.make_session_path(cache_dir, bssid)
